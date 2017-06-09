8130ccde760f35285b3867119842c61d30377b75
Alan Lai <AlanLai200@yahoo.com>
2017-06-09 02:27:47 -0400

    Remove dup

--- ch3/biggerBetterFavoriteNumber.rb
+++
@@ -1,4 +0,0 @@
-puts "What\'s your favorite number?'"
-theirNumber = gets.chomp
-betternumber = theirNumber.to_i + 1
-puts theirNumber.to_s + " is a good number, but " + betternumber.to_s + " is better."
--- ch3/fullNameGreeting.rb
+++
@@ -1,8 +0,0 @@
-puts "Hello there! Can I get your first name?"
-firstName =gets.chomp
-puts "Hello " +firstName + ". Can I get your last name?"
-lastName=gets.chomp
-puts "Wow so your name is " + firstName +" "+ lastName
-puts "I want to know your full name. Can I get your middle name?"
-middleName=gets.chomp
-puts firstName + " " + middleName + " " + lastName + " is a good name!"
